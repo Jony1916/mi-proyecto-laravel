c8a7a88ffac00c502c88fc772bf313be2de691b2
Jony1916 <141374233+Jony1916@users.noreply.github.com>
2024-06-05 21:37:13 -0600

    rutas

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ContactoController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('contacto', ContactoController::class);
+Route::get('detalles/{id}', [ContactoController::class, 'detalles']);
+Route::get('filtrar/{ciudad}', [ContactoController::class, 'filtrarPorCiudad']);
